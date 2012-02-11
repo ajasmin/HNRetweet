ajasmin/HNRetweet

commit c94dd14d45c7cd532ef2fb8d1776c6cdadad1c48

Insert the link correctly in posts you own

userscript.user.js
@@ -29,7 +29,7 @@ function retweet_link(node, href) {
       return false;
     };
     node.parentNode.insertBefore(a, node.nextSibling);
-    node.parentNode.insertBefore(document.createTextNode(" | "), node.parentNode.lastChild);
+    node.parentNode.insertBefore(document.createTextNode(" | "), a.nextSibling);
 }
 
 function add_links() {

commit cde0377ce2e6ba8db98f79e02d8011a4251839fa

Bugfix c94dd14 broke link display for post without comment link

userscript.user.js
@@ -21,15 +21,15 @@ function x(node, xpath) {
 }
 
 function retweet_link(node, href) {
-    var a = document.createElement('a');
-    a.href = href;
-    a.appendChild(document.createTextNode("retweet"));
-    a.onclick = function() {
-      window.open(this.href, "hnrtw", "width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1,left=408,top=159");
-      return false;
-    };
-    node.parentNode.insertBefore(a, node.nextSibling);
-    node.parentNode.insertBefore(document.createTextNode(" | "), a.nextSibling);
+	var a = document.createElement('a');
+	a.href = href;
+	a.appendChild(document.createTextNode("retweet"));
+	a.onclick = function() {
+		window.open(this.href, "hnrtw", "width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1,left=408,top=159");
+		return false;
+	};
+	node.parentNode.insertBefore(a, node.nextSibling);
+	node.parentNode.insertBefore(document.createTextNode(" | "), a.nextSibling || a);
 }
 
 function add_links() {