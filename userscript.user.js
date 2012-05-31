33d3fd2802f5390e529cb734ba71550541ca87e8
Alexandre Jasmin <alexandre.jasmin@gmail.com>
2012-05-31 18:58:56 -0400

    Take post id from article link when available

--- userscript.user.js
+++ userscript.user.js
@@ -36,16 +36,23 @@ function add_links() {
 	x(document, '//td[@class="title"]/../following-sibling::*[1]//text()').filter(
 		function(n) {return n.nodeValue.match(/ago( +\| *)?$/)}
 	).forEach(function(n) {
-		var title_a, qs, comment_a, id_match;
+		var title_a, qs, comment_a, id_match, id;
 		if (title_a = x(n, '../../preceding-sibling::*[1]/td[@class="title"]/a[@href]')[0]) {
 			qs = "?url=" + encodeURIComponent(title_a.href) + "&title=" + encodeURIComponent(title_a.childNodes[0].nodeValue);
-			if ((comment_a = x(n, '../a[starts-with(@href,"item\?id=")]')[0])
+			if (id_match = title_a.getAttribute('href').match(/^(http:\/\/news.ycombinator.com\/)?item\?id=(\d+)$/)) {
+					//Post id in the article link
+					id = id_match[2];
+			} else if ((comment_a = x(n, '../a[starts-with(@href,"item\?id=")]')[0])
 				&& (id_match = comment_a.getAttribute("href").match(/^item\?id=(\d+)$/))) {
-					// Have a comment link with hn id
-					retweet_link(n, "http://hn-retweet.appspot.com/retweet/" + id_match[1] + qs);
+					// Post id in the comment link
+					id = id_match[1];
+			}
+
+			if (id) {
+				retweet_link(n, "http://hn-retweet.appspot.com/retweet/" + id + qs);
 			} else {
-					// No id use article link
-					retweet_link(n, "http://hn-retweet.appspot.com/retweet" + qs);
+				// No id use article link
+				retweet_link(n, "http://hn-retweet.appspot.com/retweet" + qs);
 			}
 		}
 	});
