cac9e33eaa26741c6521b2a8b2675f4398fd94bc
Alexandre Jasmin <alexandre.jasmin@gmail.com>
2012-06-12 08:10:39 -0400

    Use form-encode for url params
    Simpler and url-encode wasn't encoding the '+' character

--- webapp/src/HNRetweet/rest.clj
+++ webapp/src/HNRetweet/rest.clj
@@ -1,24 +1,10 @@
 (ns HNRetweet.rest
-  (:use [ring.util.codec :only [url-encode]]
+  (:use [ring.util.codec :only [form-encode]]
         [clojure.tools.logging :only [warn]])
   (:require [cheshire.core :as json]))
 
-; --- http://stackoverflow.com/q/3644219
-(defn make-query-string [m & [encoding]]
-  (let [s #(if (instance? clojure.lang.Named %) (name %) %)
-        enc (or encoding "UTF-8")]
-    (->> (for [[k vs] m
-               v (if (sequential? vs) vs [vs])]
-           (str (url-encode (s k) enc)
-                "="
-                (url-encode (str v) enc)))
-         (interpose "&")
-         (apply str))))
-
-(defn build-url [url-base query-map & [encoding]]
-  (str url-base "?" (make-query-string query-map encoding)))
-
-; ---
+(defn build-url [url-base query-map]
+  (str url-base "?" (form-encode query-map)))
 
 (defn json-request [url params]
   (let [url (build-url url params)
