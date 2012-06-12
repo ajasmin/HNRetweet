e884c3776f256902b1ef3323161888f5e06da40e
Alexandre Jasmin <alexandre.jasmin@gmail.com>
2012-06-12 08:09:27 -0400

    Update dependencies

--- webapp/project.clj
+++ webapp/project.clj
@@ -1,10 +1,10 @@
 (defproject HNRetweet "1.0.0-SNAPSHOT"
   :description "Webapp portion of HNRetweet"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [ring "1.0.2"]
-                 [compojure "1.0.1"]
-                 [cheshire "2.1.0"]
+                 [ring "1.1.0"]
+                 [compojure "1.1.0"]
+                 [cheshire "4.0.0"]
                  [clj-oauth-nohttp "1.3.1-SNAPSHOT"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.6.2.1"]]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.6.6"]]
   :aot [HNRetweet.servlet])
